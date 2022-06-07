27332fe5b8156d5e250c20db1b5b10f4f09937d7
Kanat Orynbaev <81953174+Kanat000@users.noreply.github.com>
2022-06-07 19:01:05 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-bot_token = '5314499316:AAGMY9-34RsivYqwvKXvYop1fs24hCjZWEw'
-provider_token = '381764678:TEST:37206'
+bot_token = 'bot-token'
+provider_token = 'provider-token'
 dbName = 'myBot.db'
 
 SENDER_LOGIN = '<Here your email>'
 SENDER_PASSWORD = '<Here your password>'
 
-robot1 = 'robot1@robotize.pro'
-robot2 = 'robot1@robotize.pro'
+robot1 = 'robot1-email'
+robot2 = 'robot2-email'
